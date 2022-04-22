89437d09d7ab482f34c52137ab599ac992e7ab4d
SirAlyon <alessiosada04@gmail.com>
2022-04-22 15:06:40 +0200

    add milestone 3

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const app = new Vue ({
     el: '#app',
     data: {
-
+        newTask: '',
+        tasks: [
+            {
+                text: 'Learn HTML',
+                done: true,
+            },
+            {
+                text: 'Learn CSS',
+                done: true,
+            },
+            {
+                text: 'Learn JS',
+                done: false,
+            },
+            {
+                text: 'Learn VueJS',
+                done: false,
+            },
+            {
+                text: 'Learn PHP',
+                done: false,
+            },
+            {
+                text: 'Learn SQL',
+                done: false,
+            }
+        ]
     },
     methods: {
-        
+        deleteTask(index){
+            this.tasks.splice(index, 1)
+        },
+        addNewTask(){
+            const newTaskObject = {
+                text: this.newTask,
+                done: false
+            }
+            console.log(newTaskObject);
+            this.tasks.push(newTaskObject)
+        }
     }
 })
